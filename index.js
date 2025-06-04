d7ddd82bb71bd5600ddba53f31a4a3d653eec50c
Hassan kh salar <khsalarhassan@gmail.com>
2025-06-04 16:32:13 +0600

    fixed minor issue

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors({
   origin: [
-    'https://steady-dolphin-3350d4.netlify.app/',
+    'https://steady-dolphin-3350d4.netlify.app',
     'http://localhost:5173'
   ],
   credentials: true
